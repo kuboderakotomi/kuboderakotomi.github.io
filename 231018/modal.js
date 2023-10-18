9418a884f1dfdd1b68d5b5282397e2a80ca641b1
kuboderakotomi <m0a2109643@edu.teu.ac.jp>
2023-10-18 18:42:31 +0900

    2023年秋学期第1回

---
+++ 231018/modal.js
@@ -0,0 +1,18 @@
+console.log("modal.jsを読み込みました");
+
+function showModal(){
+  let element = document.querySelector("#modal");
+  element.innerHTML = "クリックされたぞ"
+}
+function changeRed(){
+  let element = document.querySelector("#modal");
+    element.style.backgroundColor = "red"
+}
+function changeYellow(){
+  let element = document.querySelector("#modal");
+  element.style.backgroundColor = "yellow"
+}
+function changeGreen(){
+  let element = document.querySelector("#modal");
+  element.style.backgroundColor = "green"
+}
