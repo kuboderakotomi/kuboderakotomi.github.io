40658fa918989c865951b66914b72c21606fdef8
kuboderakotomi <m0a2109643@edu.teu.ac.jp>
2023-10-25 17:50:23 +0900

    2023年秋学期第5回

--- 231018/kadai3.js
+++ 231018/kadai3.js
@@ -1,10 +1,10 @@
-console.log("modal.jsを読み込みました");
+console.log("kadai3.jsを読み込みました");
 function appendCircle(){
   let element = document.querySelector("#modal");
-  element.innerHTML += "○"
+  element.innerHTML += "○";
 }
 function appendCross(){
   let element = document.querySelector("#modal");
-  element.innerHTML += "×"
+  element.innerHTML += "×";
 }
 
