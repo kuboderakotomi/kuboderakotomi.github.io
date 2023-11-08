94effd3aa2a40dd2eae9633346169278f729f4ca
kuboderakotomi <m0a2109643@edu.teu.ac.jp>
2023-11-08 15:34:44 +0900

    2023年秋学期第7回

---
+++ 231108/kadai.js
@@ -0,0 +1,27 @@
+let fruitNames = ["りんご","みかん","いちご","いちじく"];
+
+function fruit() {
+  let answer = document.querySelector('#answer');
+}
+
+let arr = ["りんご","みかん","いちご","いちじく"];
+let i = 0;
+while(i < arr.length){
+  console.log(arr[i]);
+  i++;
+}
+
+let lists = ["りんご","みかん","いちご","いちじく"];
+for(let i = 0; i < lists.length; i++){
+  console.log(lists[i]);
+}
+
+
+
+
+
+
+
+
+
+
