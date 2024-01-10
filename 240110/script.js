c76d005cd0ef883cd3b3f892c56dd1cf15f3b9c2
kuboderakotomi <m0a2109643@edu.teu.ac.jp>
2024-01-10 13:29:30 +0900

    2023年秋学期最終課題

--- 240110/script.js
+++ 240110/script.js
@@ -13,7 +13,20 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-
+//scroll_effect
+$(window).scroll(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+  var scrollAnimationFunc = function () {
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 10;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
 
 
 //menu
